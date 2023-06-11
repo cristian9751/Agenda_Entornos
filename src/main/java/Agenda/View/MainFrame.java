4df7301b7dfc1cb88806083e75162ab40d142b12
casa <cristian975112@gmail.com>
2023-06-11 10:06:04 +0200

    Retocada interfaz grafica del marco principal

--- src/main/java/Agenda/View/MainFrame.java
+++ src/main/java/Agenda/View/MainFrame.java
@@ -1,29 +1,15 @@
 package Agenda.View;
 
-import Agenda.Controller.Agenda;
-import Agenda.Controller.Contacto;
-
 import javax.swing.*;
-import javax.swing.event.ListSelectionEvent;
-import javax.swing.event.ListSelectionListener;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.util.List;
 
 
+public class MainFrame extends JFrame{
 
-
-
-
-public class MainFrame{
-
-    private  JPanel mainPanel;
-    private JTextField searchTextField;
-    private JButton searchButton;
-    private JComboBox filterComboBox;
-    private JTextPane infoContactTextPane;
-    private JList contactList;
+    private  JPanel panelList;
+    private JTextField tfSearch;
+    private JButton btnSearch;
+    private JComboBox cmbxFilter;
+    private JList listContactos;
     private JScrollBar scrollBar1;
 
     private JButton deleteContactButton = new JButton();
@@ -31,6 +17,7 @@ public class MainFrame{
     private JButton newContactButton;
 
     private JButton btnReload;
+    private JPanel DataPanel;
 
 
 }
