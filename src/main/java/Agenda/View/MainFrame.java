668e210e635116e41d213a01e9d62dc276a37d4e
casa <cristian975112@gmail.com>
2023-06-11 12:18:35 +0200

    Creada parte grafica de la interfaz de usuario del frame principal

---
+++ src/main/java/Agenda/View/MainFrame.java
@@ -0,0 +1,15 @@
+package Agenda.View;
+
+import javax.swing.*;
+
+public class MainFrame {
+    private JPanel panelMain;
+    private JPanel pnlInnerNorth;
+    private JPanel pnlInnerCenter;
+    private JTextField tfSearch;
+    private JComboBox comboBox1;
+    private JButton btnSearch;
+    private JList listContact;
+    private JButton btnNewContact;
+    private JButton btnRemoveContact;
+}
